0029286fd6e4bae6d5616d96ac426704ba5f1917
Israt-Nisha <isratnisha1999@gmail.com>
2026-05-12 20:51:23 +0600

    ingestion of Doctors data

--- src/app/module/rag/rag.contoller.ts
+++ src/app/module/rag/rag.contoller.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
+import status from "http-status";
+import { catchAsync } from "../../sheard/catchAsync";
+import { sendResponse } from "../../sheard/sendResponse";
+import { RAGService } from "./rag.service";
 
+const ragService = new RAGService();
 const getStats = async (req: Request, res: Response) => {
   console.log("connected", req.query);
   res.status(200).json({ message: "connected rag apis" });
 };
 
+const ingestDoctors = catchAsync(async (req: Request, res: Response) => {
+ const result = await ragService.ingestDoctorsData();
 
+  sendResponse(res,{
+    success: true,
+    httpStatusCode: status.OK,
+    message: "Doctors data ingestion completed",
+    data: result
+  })
+});
 
 export const RagController = {
   getStats,
+  ingestDoctors,
 };
--- src/app/module/rag/rag.route.ts
+++ src/app/module/rag/rag.route.ts
@@ -5,4 +5,7 @@ const router = Router();
 
 router.get("/stats", RagController.getStats);
 
+//index doctors data
+router.post("/ingest-doctors", RagController.ingestDoctors);
+
 export const RagRoutes = router;
